1def6c3f2e5d049e9608822c2c6f9716dc8da27b
cathrkri <cathrkri@stud.ntnu.com>
2026-02-03 21:28:00 +0100

    Modified: implemented class player

---
+++ src/main/java/no/ntnu/idatt2003/group22/millions/Player.java
@@ -0,0 +1,39 @@
+package no.ntnu.idatt2003.group22.millions;
+
+import java.math.BigDecimal;
+
+public class Player {
+    private String name;
+    private BigDecimal startingMoney;
+    private BigDecimal money;
+    private Portfolio portfolio;
+    private TransactionArchive transactionArchive;
+
+    public Player(String name, BigDecimal startingMoney) {
+        this.name = name;
+        this.startingMoney = startingMoney;
+        this.money = startingMoney;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public BigDecimal getMoney() {
+        return money;
+    }
+
+    public void addMoney(BigDecimal amount) {
+        money = money.add(amount);
+    }
+    public withdrawMoney(BigDecimal amount){
+        money = money.subtract(amount);
+    }
+    public Portfolio getPortfolio() {
+        return portfolio;
+    }
+    public TransactionArchive getTransactionArchive() {
+        return transactionArchive;
+    }
+    
+
+}
