93233eb5ad05b096002da24d66ef0fb0545e8f71
meenakshijay1005-netizen <meenakshi.jay1005@gmail.com>
2026-02-23 09:59:38 +0100

    Modifications: changes added to player class

---
+++ src/main/java/no/ntnu/idatt2003/group22/millions/Purchase2.java
@@ -0,0 +1,33 @@
+package no.ntnu.idatt2003.group22.millions;
+
+import java.math.BigDecimal;
+import java.util.Objects;
+
+public class Purchase2 extends Transaction2{
+    public Purchase2(Share share, int week){
+        super(share, week, new PurchaseCalculator(share));
+    }
+
+    @Override
+    protected void doCommit(Player player){
+        Objects.requireNonNull(player, "player can not be null");
+
+        // finner total kostnad
+        BigDecimal totalCost = getCalculator().calculateNetAmount();
+
+        // sjekker at brukeren har nok penger
+        if(player.getCash().compareTo(TotalCost) < 0){
+            throw new IllegalStateException("Not enough cash to complete purchase");
+        }
+
+        // trekker penger
+        player.setCash(player.getCash().subtract(totalCost));
+
+        // legger akjsen i portoføljen
+        player.getPortofolio().addShare(getShare());
+
+        // arkiverer transaksjonen
+        player.getTransactionArchive().add(this);
+    }
+    
+}
