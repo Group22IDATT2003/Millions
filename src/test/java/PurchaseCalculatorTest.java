bd8366480f0f7f2f2be6153c491e507bb1545a8d
meenakshijay1005-netizen <meenakshi.jay1005@gmail.com>
2026-02-27 12:11:16 +0100

    Modifications: PurchaseCalculatorTest

---
+++ src/test/java/PurchaseCalculatorTest.java
@@ -0,0 +1,36 @@
+package no.ntnu.idatt2003.group22.millions;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DisplayName;
+import java.math.BigDecimal;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Tests PurchaseCalculator class
+ */
+public class PurchaseCalculatorTest {
+    private PurchaseCalculator calculator;
+
+    @BeforeEach
+    void SetUp(){
+        Stock stock = new Stock("AAPL", "Apple Incoporations", new BigDecimal("100"));
+        Share share = new Share(stock, new BigDecimal("10"), new BigDecimal(100));
+        calculator = new PurchaseCalculator(share);
+    }
+
+    @Test
+    @DisplayName("calculatingGross: buying_price + amount")
+    void calculateGross_validShare_returnsCorrectGross(){
+        BigDecimal gross = calculator.calculateGross();
+        assertEquals(new BigDecimal("1000"), gross);
+    }
+
+    @Test
+    @DisplayName("calculatingCommision: 0.5% og brutto of purchase")
+    void calculateCommission_validShare_returnsCorrectCommision(){
+        BigDecimal commision = calculator.calculateCommission();
+        assertEquals(new BigDecimal("5.00"))
+    }
+    
+}
