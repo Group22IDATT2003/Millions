f10b81edf860cd3bba37ad0cb0718a3063e10f30
cathrkri <cathrkri@stud.ntnu.com>
2026-03-18 23:39:49 +0100

    feat: the class stockFileHandler created

---
+++ src/main/java/no/ntnu/idatt2003/group22/millions/StockFileHandler.java
@@ -0,0 +1,15 @@
+package no.ntnu.idatt2003.group22.millions;
+
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+
+public class StockFileHandler {
+
+    public List<Stock> readStockFromFile(Path path){
+        List<Stock> stocks = new ArrayList<>();
+
+        try ()
+
+    }
+}
