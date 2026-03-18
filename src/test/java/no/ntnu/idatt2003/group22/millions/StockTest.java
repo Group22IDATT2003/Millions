a5b14a06cea74fb5add5e32e4902dcb12f8387dd
cathrkri <cathrkri@stud.ntnu.com>
2026-03-18 23:20:15 +0100

    feat: testclass for stock created

--- src/test/java/no/ntnu/idatt2003/group22/millions/StockTest.java
+++ src/test/java/no/ntnu/idatt2003/group22/millions/StockTest.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.DisplayName;
 import java.math.BigDecimal;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class StockTest {
@@ -26,14 +28,90 @@ public class StockTest {
     }
 
     @Test
-    @DisplayName("Konstruktør: setter symbol riktig")
-    void constructor_setsSymbolCorrectly(){
-        String symbol = stock.getSymbol();
-
-        assertEquals("AAPL", symbol);
-
+    @DisplayName("constuctor: creates stock correctly")
+    void constructorCreatesStockCorrectly() {
+        assertEquals("AAPL", stock.getSymbol());
+        assertEquals("Apple", stock.getCompany());
+        assertEquals(new BigDecimal("100"), stock.getSalesPrice());
     }
 
+    @Test
+    @DisplayName("constructor: throws exception when price is less than or equal to zero")
+    void constructorThrowsExceptionWhenSymbolIsNull() {
+        assertThrows(IllegalArgumentException.class,
+                () -> new Stock(null, "Apple", new BigDecimal("100")));
+    }
 
-    
+    @Test
+    @DisplayName("constructor: throws exception when symbol is empty")
+    void constructorThrowsExceptionWhenCompanyIsNull() {
+        assertThrows(IllegalArgumentException.class,
+                () -> new Stock("AAPL", null, new BigDecimal("100")));
+    }
+
+    @Test
+    @DisplayName("constructor: throws exception when company is empty")
+    void constructorThrowsExceptionWhenPriceIsNull() {
+        assertThrows(IllegalArgumentException.class,
+                () -> new Stock("AAPL", "Apple", null));
+    }
+
+    @Test
+    @DisplayName("constructor: throws exception when price is empty")
+    void constructorThrowsExceptionWhenPriceIsZeroOrNegative() {
+        assertThrows(IllegalArgumentException.class,
+                () -> new Stock("AAPL", "Apple", BigDecimal.ZERO));
+    }
+
+    @Test
+    @DisplayName("addNewSalesPrice: adds new price")
+    void addNewSalesPriceAddsPrice() {
+        stock.addNewSalesPrice(new BigDecimal("120"));
+
+        assertEquals(new BigDecimal("120"), stock.getSalesPrice());
+    }
+
+    @Test
+    @DisplayName("addNewSalesPrice: throws exception when price is empty")
+    void addNewSalesPriceThrowsExceptionWhenNull() {
+        assertThrows(IllegalArgumentException.class,
+                () -> stock.addNewSalesPrice(null));
+    }
+
+    @Test
+    @DisplayName("addNewSalesPrice: throws exception when price is less than or equal to zero")
+    void addNewSalesPriceThrowsExceptionWhenNegative() {
+        assertThrows(IllegalArgumentException.class,
+                () -> stock.addNewSalesPrice(new BigDecimal("-10")));
+    }
+
+    @Test
+    @DisplayName("getHistoricalPrices: returns copy ")
+    void getHistoricalPricesReturnsCopy() {
+        List<BigDecimal> prices = stock.getHistoricalPrices();
+
+        assertEquals(1, prices.size());
+
+        assertThrows(UnsupportedOperationException.class,
+                () -> prices.add(new BigDecimal("200")));
+    }
+
+    @Test
+    @DisplayName("getHighestAndLowestPrice: returns highest and lowest price")
+    void getHighestAndLowestPriceWorks() {
+        stock.addNewSalesPrice(new BigDecimal("120"));
+        stock.addNewSalesPrice(new BigDecimal("80"));
+
+        assertEquals(new BigDecimal("120"), stock.getHighestPrice());
+        assertEquals(new BigDecimal("80"), stock.getLowestPrice());
+    }
+
+    @Test
+    @DisplayName("getLatestPriceChange: returns difference between latest price and previous price")
+    void getLatestPriceChangeReturnsCorrectDifference() {
+        stock.addNewSalesPrice(new BigDecimal("120"));
+
+        assertEquals(new BigDecimal("20"), stock.getLatestPriceChange());
+    }
+
 }
