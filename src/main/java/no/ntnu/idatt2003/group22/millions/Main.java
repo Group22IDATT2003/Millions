70f7789a53d56f4a43e3333e553688e53f2e8787
cathrkri <cathrkri@stud.ntnu.com>
2026-03-25 15:36:07 +0100

    feat: update class filehandler and main

--- src/main/java/no/ntnu/idatt2003/group22/millions/Main.java
+++ src/main/java/no/ntnu/idatt2003/group22/millions/Main.java
@@ -1,7 +1,15 @@
 package no.ntnu.idatt2003.group22.millions;
 
-public class Main {
-    public static void main(String [] args){
+import java.io.IOException;
+import java.util.List;
 
+public class Main {
+    public static void main(String [] args) throws IOException {
+        StockFileHandler handler = new StockFileHandler();
+        List<Stock> stocks = handler.readStocksFromFile("sp500.csv");
+
+        Exchange exchange = new Exchange("NASDAQ", stocks);
+
+        System.out.println("Loaded stocks: " + stocks.size());
     }
 }
