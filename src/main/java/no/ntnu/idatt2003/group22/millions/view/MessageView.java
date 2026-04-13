55938784ab50e898b2d376f2c33ac3812f5acd80
unknown <cathrkri@stud.ntnu.no>
2026-04-13 23:34:49 +0200

    feat: created metods i view

--- src/main/java/no/ntnu/idatt2003/group22/millions/view/MessageView.java
+++ src/main/java/no/ntnu/idatt2003/group22/millions/view/MessageView.java
@@ -1,4 +1,41 @@
 package no.ntnu.idatt2003.group22.millions.view;
 
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.VBox;
+
 public class MessageView {
+    private final VBox root;
+    private final Label titleLabel;
+    private final TextArea messageArea;
+
+    public MessageView(){
+        this.root = new VBox();
+
+        this.titleLabel = new Label("Messages");
+        this.messageArea = new TextArea("");
+
+        configureLayout();
+    }
+
+    private void configureLayout(){
+        root.setSpacing(20);
+        root.setPadding(new Insets(15));
+        root.setAlignment(Pos.CENTER_LEFT);
+        messageArea.setEditable(false);
+        messageArea.setWrapText(true);
+        root.getChildren().addAll(titleLabel, messageArea);
+    }
+
+    public VBox getRoot(){
+        return root;
+    }
+
+    public void updateMessageInfo(String title, String message){
+        titleLabel.setText("Messages: " + title);
+        messageArea.setText(message);
+    }
+
 }
--- src/main/java/no/ntnu/idatt2003/group22/millions/view/TopBarView.java
+++ src/main/java/no/ntnu/idatt2003/group22/millions/view/TopBarView.java
@@ -1,24 +1,57 @@
 package no.ntnu.idatt2003.group22.millions.view;
 
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.layout.HBox;
 
-import javax.lang.model.element.Name;
-import java.lang.classfile.Label;
+import javafx.scene.control.Label;
+
+import java.math.BigDecimal;
 
 public class TopBarView {
-    private HBox root;
-    private Label playerNameLabel;
-    private Label weekLabel;
-    private Label moneyLabel;
-    private Label netWorthLabel;
-    private Label statusLabel;
+    private final HBox root;
+    private final Label playerNameLabel;
+    private final Label weekLabel;
+    private final Label moneyLabel;
+    private final Label netWorthLabel;
+    private final Label statusLabel;
 
     public TopBarView(){
+        this.root = new HBox();
 
+        this.playerNameLabel = new Label("Player: -");
+        this.weekLabel = new Label("Week: -");
+        this.moneyLabel = new Label("Money: -");
+        this.netWorthLabel = new Label("Net worth: -");
+        this.statusLabel = new Label("Status: -");
+
+        configureLayout();
     }
 
     private void configureLayout(){
+        root.setSpacing(20);
+        root.setPadding(new Insets(15));
+        root.setAlignment(Pos.CENTER_LEFT);
 
+        root.getChildren().addAll(
+                playerNameLabel,
+                weekLabel,
+                moneyLabel,
+                netWorthLabel,
+                statusLabel
+        );
+    }
+
+    public HBox getRoot(){
+        return root;
+    }
+
+    public void updatePlayerInfo(String playerName, int week, BigDecimal money, BigDecimal netWorth, String status){
+        playerNameLabel.setText("Player: " + playerName);
+        weekLabel.setText("Week: " + week);
+        moneyLabel.setText("Money: " + money);
+        netWorthLabel.setText("Net worth: " + netWorth);
+        statusLabel.setText("Status: " + status);
     }
 
 }
