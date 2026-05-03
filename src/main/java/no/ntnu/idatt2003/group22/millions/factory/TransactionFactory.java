ed9ca26a12f6d00f662364418c509630eafee17b
cathrkri <cathrkri@stud.ntnu.com>
2026-05-03 21:49:24 +0200

    feat: Transaction

--- src/main/java/no/ntnu/idatt2003/group22/millions/factory/TransactionFactory.java
+++
@@ -1,4 +0,0 @@
-package no.ntnu.idatt2003.group22.millions.factory;
-
-public class TransactionFactory {
-}
--- src/main/java/no/ntnu/idatt2003/group22/millions/observer/GameObserver.java
+++
@@ -1,4 +0,0 @@
-package no.ntnu.idatt2003.group22.millions.observer;
-
-public class GameObserver {
-}
